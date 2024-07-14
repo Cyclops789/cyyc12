2343cf13b5f61409432ee90b973b9fd655c91de4
Cyclops789 <70551889+Cyclops789@users.noreply.github.com>
2024-07-14 03:39:14 +0100

    Replace label and force focus using js

--- src/components/Terminal/TerminalLabel.tsx
+++ src/components/Terminal/TerminalLabel.tsx
@@ -5,11 +5,12 @@ type Props = { children: React.ReactNode }
 
 function TerminalLabel({ children }: Props) {
     return (
-        <label htmlFor="terminal">
-            <div css={tw`bg-black w-full h-full cursor-text overflow-auto rounded-b-lg select-all`}>
-                {children}
-            </div>
-        </label>
+        <div 
+            css={tw`bg-black w-full h-full cursor-text overflow-auto rounded-b-lg select-text`}
+            onClick={() => document.getElementById("terminal")!.focus()}    
+        >
+            {children}
+        </div>
     )
 }
 
